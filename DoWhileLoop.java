d9d4c335da97e7b08c7d42b9d8cb8ec8d8fe8d08
Sudhanshu264 <102288681+Sudhanshu264@users.noreply.github.com>
2023-02-22 16:02:56 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int j=0;
+        do{
+            System.out.println("sudh");
+            j++;
+        }
+        while(j<5);
+        
+    }
+    
+}
